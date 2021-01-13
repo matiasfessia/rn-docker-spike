4906bf877921112c3157c09ac9e4a5fa0e005175
Matias Fessia <matiasfessia@gmail.com>
2021-01-13 15:14:27 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>React Native running on Docker, isn't cool? 😎</Text>
+    </>
+  );
+};
+
+export default App;
